3dc99f4caa702484b1c0357729c0ca8375c6f915
timhuang1018 <t8522192@gmail.com>
2018-05-29 08:41:14 +0800

    show data not finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   	def after_sign_in_path_for(resource)
-  		managers_path(current_user) #your path
+  		a = managers_path(current_user)
+  		p a  		
+  		managers_path #your path
 	end
 end
