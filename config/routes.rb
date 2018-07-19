8742793f772fd3d75e522bb9bab06f087cc2c973
timhuang1018 <t8522192@gmail.com>
2018-07-19 21:42:16 +0800

    routes modified

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get"/", to: "pages#hello"
+
 
 end
